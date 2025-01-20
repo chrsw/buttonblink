37149e47ad0ea24b3fd31cfc32578abf2c97b1a2
chrsw <chrsw@users.noreply.github.com>
2025-01-19 23:56:37 -0500

    Turn on LED
    Untested

--- main.c
+++ main.c
@@ -3,6 +3,20 @@
 #include "stm32f4xx.h"
 #include "core_cm4.h"
 
-int main(void) {
-	return 0;
+// LED pin
+#define GPIOAEN						(1U<<0)
+#define PIN5						(1U<<5)
+#define LED_PIN						PIN5
+
+
+int main(void) 
+{
+	// enable GPIOA clock
+	RCC->AHB1ENR |= GPIOAEN;
+	// set PA5 to output
+	GPIOA->MODER |= (1U<<10);
+	GPIOA->MODER &= ~(1U<<11);
+
+	// turn on LED
+	GPIOA->ODR |= LED_PIN;
 }
